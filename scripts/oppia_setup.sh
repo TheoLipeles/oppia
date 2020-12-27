15e143d745e3048b430db5427522372f7477bbea
Theo Lipeles <theolipeles@google.com>
2020-12-26 23:45:52 -0500

    add copyright

--- scripts/oppia_setup.sh
+++ scripts/oppia_setup.sh
@@ -1,6 +1,27 @@
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' #No color
+# Copyright 2020 The Oppia Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##########################################################################
+
+# INSTRUCTIONS:
+#
+# This script sets up aditional prerequisites for Oppia
+# It's better if you copy this file somewhere outside this project, e.g.
+#     cp scripts/oppia_setup.sh ~/opensource
+# Run the script from wherever with:
+#     bash oppia_setup.sh
+
 function extra_setup_mac {
   if ( python --version | grep -v -q "2.7" ); then
     echo "Python 2.7 not detected"
